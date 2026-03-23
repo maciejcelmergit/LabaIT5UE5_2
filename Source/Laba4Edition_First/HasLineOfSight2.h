f79e1cbeb0e8c132be9ff174292888b32a353cf3
maciejcelmergit <maciej.m.celmer@gmail.com>
2026-03-23 23:57:02 +0100

    26. Perception, EQS

---
+++ Source/Laba4Edition_First/HasLineOfSight2.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "HasLineOfSight2.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LABA4EDITION_FIRST_API UHasLineOfSight2 : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+};
